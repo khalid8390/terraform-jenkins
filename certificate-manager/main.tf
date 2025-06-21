2fdfea086953bc07c7f9d55ac0b72b74a540ff37
khalid8390 <139584495+khalid8390@users.noreply.github.com>
2025-06-21 16:13:21 +0530

    Update main.tf

--- certificate-manager/main.tf
+++ certificate-manager/main.tf
@@ -2,35 +2,18 @@ variable "domain_name" {}
 variable "hosted_zone_id" {}
 
 output "dev_proj_1_acm_arn" {
-  value = aws_acm_certificate.dev_proj_1_acm_arn.arn
+  value = arn:aws:acm:ap-south-1:072461344079:certificate/ace18ec3-945e-4634-9d1e-e47592814fd3
 }
 
 resource "aws_acm_certificate" "dev_proj_1_acm_arn" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
-  tags = {
-    Environment = "production"
-  }
-
-  lifecycle {
-    create_before_destroy = false
-  }
+ data "aws_acm_certificate" "existing_cert" {
+  domain   = "oyester.icu"
+  statuses = ["ISSUED"]   # You can also use ["PENDING_VALIDATION"]
+  most_recent = true
 }
 
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.dev_proj_1_acm_arn.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  zone_id = var.hosted_zone_id # replace with your Hosted Zone ID
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
 }
 
