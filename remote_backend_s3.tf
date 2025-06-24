cf32a022a21fc448db697b62213703c96a989a95
khalid8390 <139584495+khalid8390@users.noreply.github.com>
2025-06-24 11:43:24 +0530

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "suhailkhalid"
+    bucket = "suhailkhalidsuhail"
     key    = "devops-project-1/jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
